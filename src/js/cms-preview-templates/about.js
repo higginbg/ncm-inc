6d48763faeb5502ce8d71fbbd901950a92cb0f91
higginbg <30296062+higginbg@users.noreply.github.com>
2020-01-28 09:18:24 -0800

    Update about.js

--- src/js/cms-preview-templates/about.js
+++ src/js/cms-preview-templates/about.js
@@ -7,14 +7,19 @@ export default class AboutPreview extends React.Component {
   render() {
     const {entry, getAsset} = this.props;
     
-    const title = getAsset(entry.getIn(["data", "title"]));
-    const subtitle = getAsset(entry.getIn(["data", "subtitle"]));
-    const image = getAsset(entry.getIn(["data", "image"]));
+    let image = getAsset(entry.getIn(["data", "image"]));
+
+    if (image && !image.fileObj) {
+      image = window.parent.location.protocol + "//" + window.parent.location.host + image;
+    }
+
+    const name = getAsset(entry.getIn(["data", "title"]));
+    const position = getAsset(entry.getIn(["data", "subtitle"]));
     
     return <div>
       <div className="bg-off-white pv4">
         <div className="mw7 center ph3 pt4">
-          <MediaTile title={title} subtitle={subtitle} image={image} />
+          <MediaTile title={name} subtitle={position} image={image} />
         </div>
       </div>
     </div>;
