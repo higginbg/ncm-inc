bd2d35df41b3953c12874829ee129c84b2a22e1d
higginbg <30296062+higginbg@users.noreply.github.com>
2020-01-28 11:59:17 -0800

    Update about.js

--- src/js/cms-preview-templates/about.js
+++ src/js/cms-preview-templates/about.js
@@ -1,33 +1,33 @@
-import React from "react";
+import React from 'react';
 
 const { useState } = React;
 
-export default class AboutPreview extends React.Component {
-  render() {
-    const {entry, getAsset} = this.props;
+export default AboutPreview => {
+  const [image, setImage] = useState('');
 
-    const [image, setImage] = useState('');
+  const { entry, getAsset } = this.props;
 
-    const name = entry.getIn(["data", "title"]);
-    const position = entry.getIn(["data", "subtitle"]);
-    const body = entry.getIn(["data", "body"]);
+  const name = entry.getIn(['data', 'title']);
+  const position = entry.getIn(['data', 'subtitle']);
+  const body = entry.getIn(['data', 'body']);
 
-    getAsset(entry.getIn(["data", "image"]))
-      .then(path => setImage(path.toString()));
-    
-    return <div class="mw6 center ph3 pv4">
-    <div class="flex flex-row flex-wrap">
+  getAsset(entry.getIn(['data', 'image'])).then(path =>
+    setImage(path.toString())
+  );
 
-      <div class="w-50-ns">
-        <h1 class="f2 lh-title mb2">{name}</h1>
-        <h2 class="f4 lh-title mb3">{position}</h2>
-        <div class="cms">{body}</div>
-      </div>
+  return (
+    <div class='mw6 center ph3 pv4'>
+      <div class='flex flex-row flex-wrap'>
+        <div class='w-50-ns'>
+          <h1 class='f2 lh-title mb2'>{name}</h1>
+          <h2 class='f4 lh-title mb3'>{position}</h2>
+          <div class='cms'>{body}</div>
+        </div>
 
-      <div class="w-50-ns w-100 pl3-ns">
-        <img src={image} class="w-100 h5 br1 object-cover"/>
+        <div class='w-50-ns w-100 pl3-ns'>
+          <img src={image} class='w-100 h5 br1 object-cover' />
+        </div>
       </div>
     </div>
-  </div>;
-  }
-}
+  );
+};
