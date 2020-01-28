f430b97c177e6a3355b0a6c53fd24edc9269849b
higginbg <30296062+higginbg@users.noreply.github.com>
2020-01-28 10:20:02 -0800

    Update about.js

--- src/js/cms-preview-templates/about.js
+++ src/js/cms-preview-templates/about.js
@@ -9,20 +9,33 @@ export default class AboutPreview extends React.Component {
 
     const name = entry.getIn(["data", "title"]);
     const position = entry.getIn(["data", "subtitle"]);
+    const body = entry.getIn(["data", "body"]);
     
     let image = getAsset(entry.getIn(["data", "image"]));
+
+    console.log(image);
 
     // Bit of a nasty hack to make relative paths work as expected as a background image here
     if (image && !image.fileObj) {
       image = window.parent.location.protocol + "//" + window.parent.location.host + image;
     }
+
+    console.log(image);
     
-    return <div>
-      <div className="bg-off-white pv4">
-        <div className="mw7 center ph3 pt4">
-          <MediaTile title={name} subtitle={position} image={image} />
-        </div>
+    return <div class="mw6 center ph3 pv4">
+    <div class="flex flex-row flex-wrap">
+
+      <div class="w-50-ns">
+        <h1 class="f2 lh-title mb2">{name}</h1>
+        <h2 class="f4 lh-title mb3">{position}</h2>
+        <div class="cms">{body}}</div>
       </div>
-    </div>;
+      
+      <div class="w-50-ns w-100 pl3-ns">
+        <img src={image} class="w-100 h5 br1 object-cover"/>
+      </div>      
+
+    </div>
+  </div>;
   }
 }
