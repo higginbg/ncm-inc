8044b750ac04d3d8f75fe3e10b7ba26ed49fc50f
higginbg <30296062+higginbg@users.noreply.github.com>
2020-01-28 09:09:22 -0800

    About Page CMS

--- src/js/cms-preview-templates/about.js
+++ src/js/cms-preview-templates/about.js
@@ -1,44 +1,20 @@
 import React from "react";
-import { List } from 'immutable';
 
-import Jumbotron from "./components/jumbotron";
+import MediaTile from "./components/media-tile";
 
-const MediaBlock = ({heading, text, imageUrl, reverse}) => {
-  const imageContainerClassName = reverse
-    ? "ph3-m w-50-m"
-    : "ph3-m w-50-m order-last-m";
-  return <div className="flex-m mhn3-m mb4">
-    <div className={imageContainerClassName}>
-      <img src={imageUrl} alt="" className="db mb2" />
-    </div>
-    <div className="ph3-m w-50-m">
-      <h3 className="f3 b lh-title mb1">{heading}</h3>
-      <p>{text}</p>
-    </div>
-  </div>;
-};
 
 export default class AboutPreview extends React.Component {
   render() {
     const {entry, getAsset} = this.props;
     
-    let image = getAsset(entry.getIn(["data", "image"]));
-
-    // Bit of a nasty hack to make relative paths work as expected as a background image here
-    if (image && !image.fileObj) {
-      image = window.parent.location.protocol + "//" + window.parent.location.host + image;
-    }
-    
-    const entryValues = entry.getIn(["data", "values"]);
-    const values = entryValues ? entryValues.toJS() : [];
+    const title = getAsset(entry.getIn(["data", "title"]));
+    const subtitle = getAsset(entry.getIn(["data", "subtitle"]));
+    const image = getAsset(entry.getIn(["data", "image"]));
     
     return <div>
-      <Jumbotron image={image} title={entry.getIn(["data", "title"])} />
       <div className="bg-off-white pv4">
         <div className="mw7 center ph3 pt4">
-          {values.map(({text, heading, imageUrl}, i) =>
-            <MediaBlock key={i} text={text} heading={heading} imageUrl={imageUrl} reverse={i % 2 === 0} />
-          )}
+          <MediaTile title={title} subtitle={subtitle} image={image} />
         </div>
       </div>
     </div>;
