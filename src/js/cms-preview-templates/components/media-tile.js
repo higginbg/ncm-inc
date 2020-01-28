e6b8b9e5493c001606a4cc435637f26dc671ea50
higginbg <30296062+higginbg@users.noreply.github.com>
2020-01-28 09:46:09 -0800

    Update media-tile.js

--- src/js/cms-preview-templates/components/media-tile.js
+++ src/js/cms-preview-templates/components/media-tile.js
@@ -4,7 +4,7 @@ export default class MediaTile extends React.Component {
   render() {
     const { title, subtitle, image } = this.props;
     return (
-      <div className="h5 mb3 br1 overflow-hidden cover relative box-shadow bg-center" style={`background-image: url('${image}')`}>
+      <div className="h5 mb3 br1 overflow-hidden cover relative box-shadow bg-center" style={{backgroundImage: `url(${image})`}}>
         <div className="banner bg-black white w-100 o-90 ph2 pv1 absolute bottom-0">
           <h2 className="f5">{title}</h2>
           <h3 className="f6">{subtitle}</h3>
