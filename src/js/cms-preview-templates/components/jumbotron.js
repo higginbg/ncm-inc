a6f3a63707ffeaadc4e8a070419adae50cf9afc5
higginbg <30296062+higginbg@users.noreply.github.com>
2020-02-04 18:47:01 -0800

    CMS Update

--- src/js/cms-preview-templates/components/jumbotron.js
+++ src/js/cms-preview-templates/components/jumbotron.js
@@ -1,25 +1,22 @@
 import React from "react";
 
-export default class Jumbotron extends React.Component {
-  render() {
-    const {image, title, subtitle} = this.props;
-    return <div>
-      <div className="pv5 pv6-l ph3 vh-75-ns vh-50 bg-center cover no-select" style={{
-        backgroundImage: image && `url(${image})`
-      }}>
-        <div className="dib mw7 center ph3">
-          <div className="bg-black o-90 br1 white pa3 mb3">
-            <div className="mw7 relative bg-fix-primary">
-              <h1 className="f2 f1-l di lh-title mb3 mw6">
-                { title }
-              </h1>
-            </div>
-            <div className="mw7 relative bg-fix-primary">
-              {subtitle && <p className="b f4 di lh-title mb3 mw6">{ subtitle }</p>}
-            </div>
-          </div>
+const Jumbotron = ({ image, title, subtitle }) => <div>
+  <div className="pv5 pv6-l ph3 vh-75-ns vh-50 bg-center cover no-select" style={{
+    backgroundImage: image && `url(${image})`
+  }}>
+    <div className="dib mw7 center ph3">
+      <div className="bg-black o-90 br1 white pa3 mb3">
+        <div className="mw7 relative bg-fix-primary">
+          <h1 className="f2 f1-l di lh-title mb3 mw6">
+            { title }
+          </h1>
+        </div>
+        <div className="mw7 relative bg-fix-primary">
+          {subtitle && <p className="f4 di lh-title mb3 mw6">{ subtitle }</p>}
         </div>
       </div>
-    </div>;
-  }
-}
+    </div>
+  </div>
+</div>;
+
+export default Jumbotron;
--- src/js/cms-preview-templates/projects-landing.js
+++ src/js/cms-preview-templates/projects-landing.js
@@ -5,6 +5,7 @@ const ProjectsLandingPreview = ({ entry }) => {
 
   const data = entry.getIn(['data']);
   const intro = data.getIn(['intro']);
+  const main = data.getIn(['main']);
 
   return <div className="bg-off-white pb4">
     <Jumbotron image={ data.getIn(['image']) } title={ data.getIn(['title']) } />
@@ -13,6 +14,13 @@ const ProjectsLandingPreview = ({ entry }) => {
       <div className='mw6 center'>
         <h1 className='f2 lh-title mb3'>{ intro.getIn(['heading']) }</h1>
         <p>{ intro.getIn(['text']) }</p>
+      </div>
+    </div>
+
+    <div className="bg-white ph3 pv4">
+      <div className='mw6 center'>
+        <h1 className='f2 lh-title mb3'>{ main.getIn(['heading']) }</h1>
+        <p>{ main.getIn(['text']) }</p>
       </div>
     </div>
 
