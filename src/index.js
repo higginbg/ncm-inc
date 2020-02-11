3832baadeed521ae8b984e55e24b9da609b3863c
higginbg <30296062+higginbg@users.noreply.github.com>
2020-02-11 02:13:36 -0800

    Update 22

--- src/index.js
+++ src/index.js
@@ -122,3 +122,48 @@ for (const link of links) {
     }
   }
 }
+
+
+/* Handle swiping left to close menu */
+/* Found here: https://stackoverflow.com/questions/15084675/how-to-implement-swipe-gestures-for-mobile-devices */
+
+let xDown = null;
+let yDown = null;
+
+const handleTouchStart = e => {
+  xDown = e.touches[0].clientX;
+  yDown = e.touches[0].clientY;
+};
+
+const handleTouchMove = e => {
+  if (!xDown || !yDown) {
+    return;
+  }
+  var xUp = e.touches[0].clientX;
+  var yUp = e.touches[0].clientY;
+  var xDiff = xDown - xUp;
+  var yDiff = yDown - yUp;
+
+  if ( Math.abs(xDiff) > Math.abs(yDiff) ) { /*most significant*/
+
+    if (xDiff > 0) {
+      /* left swipe */
+      closeMenu();
+    } else {
+      /* right swipe */
+    }
+  } else {
+    if (yDiff > 0) {
+      /* up swipe */
+    } else {
+      /* down swipe */
+    }
+  }
+
+  /* reset values */
+  xDown = null;
+  yDown = null;
+};
+
+document.addEventListener('touchstart', handleTouchStart, false);
+document.addEventListener('touchmove', handleTouchMove, false);
