4d9d5b7acf3010c4a20f0177c4bb3c2f649aeaba
higginbg <30296062+higginbg@users.noreply.github.com>
2020-02-12 21:44:15 -0800

    Update webpack.common.js

--- webpack.common.js
+++ webpack.common.js
@@ -29,7 +29,7 @@ module.exports = {
       },
       {
         test: /\.(sa|sc|c)ss$/,
-        exclude: /node_modules\/(?!(aos|lightgallery)\/).*/,
+        exclude: /node_modules\/(?!(aos)\/).*/,
         use: ["style-loader", MiniCssExtractPlugin.loader, "css-loader", "postcss-loader", "sass-loader"]
       }
     ]
