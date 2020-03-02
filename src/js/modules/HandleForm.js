a547b263f16f58669d0b5168eeabef3bd4b8f0db
higginbg <30296062+higginbg@users.noreply.github.com>
2020-03-02 01:10:33 -0800

    Form submission fix

--- src/js/modules/HandleForm.js
+++ src/js/modules/HandleForm.js
@@ -1,4 +1,3 @@
-import axios from 'axios';
 import swal from 'sweetalert';
 
 import { form } from '../variables';
@@ -9,41 +8,60 @@ if (form) {
 
   const inputs = form.querySelectorAll('[required]');
 
+  const urlencodeFormData = data => {
+    let s = '';
+    const encode = s => encodeURIComponent(s).replace(/%20/g, '+');
+    for (var pair of data.entries()) {
+      if (typeof pair[1] == 'string') {
+        s += (s ? '&' : '') + encode(pair[0]) + '=' + encode(pair[1]);
+      }
+    }
+    return s;
+  };
+
   const submitForm = e => {
     const data = new FormData(form);
     e.preventDefault();
-    axios.post(form.getAttribute('action'), data)
+    fetch('/', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      body: `form-name=${form.id}` + urlencodeFormData(data)
+    })
       .then(resp => {
         console.log(resp);
         swal('Your message has been sent!', {
+          title: 'Success!',
           icon: 'success',
         });
       })
       .catch(err => {
+        console.log(err);
         swal('\nAn error has occured. Please contact us via phone or email.\n\nSorry for any inconvenience.', {
           title: 'Error sending message',
           icon: 'error',
         });
-        console.log(err);
       });
   };
 
   handleForm = e => {
     let flag = false;
     for (const input of inputs) {
-      if (input.value.trim() === '') {
+      const { id, value } = input;
+
+      if (value.trim() === '') {
         e.preventDefault();
         flag = true;
-        swal('Oops!', `Please provide a value for '${input.id}'.`, 'error');
+        swal('Oops!', `Please provide your ${id}.`, 'error');
         break;
       }
 
-      if (input.id === 'email') {
-        if (!(/^\S+@\S+$/).test(input.value)) {
+      if (id === 'email') {
+        if (!(/^\S+@\S+$/).test(value)) {
           e.preventDefault();
+          flag = true;
           swal({
             title: 'Submit?',
-            text: '\nYour email is not properly formatted.\n\nWould you still like to submit?',
+            text: `\nYour email '${value}' is not properly formatted.\n\nWould you still to submit anyway?`,
             icon: 'info',
             buttons: true,
           }).then(willSubmit => {
