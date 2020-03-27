95b7fa9dcee83fa850900020c5d774a1d2e3b269
higginbg <30296062+higginbg@users.noreply.github.com>
2020-03-27 14:29:54 -0700

    Fewer CSS files

--- src/index.js
+++ src/index.js
@@ -6,7 +6,10 @@ import lozad from 'lozad';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 
+import '@fortawesome/fontawesome-free/js/all.min.js';
+
 import 'lightgallery.js/dist/js/lightgallery.min.js';
+import 'lightgallery.js/dist/css/lightgallery.min.css';
 import 'lg-zoom.js/dist/lg-zoom.min.js';
 import 'lg-thumbnail.js/dist/lg-thumbnail.min.js';
 
